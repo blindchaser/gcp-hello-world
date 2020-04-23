f4f2ebb6b47986070f62e4a334485fe916b519ce
Cheng <14067581+fzshot@users.noreply.github.com>
2020-04-23 16:29:09 -0400

    cloud storage

--- src/main/java/com/sabre/labs/gcpdemo/GcpDemoApplication.java
+++ src/main/java/com/sabre/labs/gcpdemo/GcpDemoApplication.java
@@ -1,37 +1,15 @@
 package com.sabre.labs.gcpdemo;
 
-import com.sabre.labs.gcpdemo.spanner.SpannerSchemaTool;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @EnableFeignClients
 @SpringBootApplication
-@EnableRedisRepositories
-@RequiredArgsConstructor
 public class GcpDemoApplication {
-
-    private final SpannerSchemaTool spannerSchemaTool;
 
     public static void main(String[] args) {
         SpringApplication.run(GcpDemoApplication.class, args);
     }
 
-    @Bean
-    public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<?, ?> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory);
-        return template;
-    }
-
-    @Bean
-    ApplicationRunner applicationRunner() {
-        return (args) -> this.spannerSchemaTool.setUp();
-    }
 }
--- src/main/java/com/sabre/labs/gcpdemo/controller/COVID19Controller.java
+++ src/main/java/com/sabre/labs/gcpdemo/controller/COVID19Controller.java
@@ -1,34 +1,67 @@
 package com.sabre.labs.gcpdemo.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.gax.paging.Page;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.Storage;
 import com.sabre.labs.gcpdemo.feign.Covid19FeignClient;
-import com.sabre.labs.gcpdemo.spanner.repository.CasesInAllUSStateRepository;
-import com.sabre.labs.gcpdemo.spanner.table.CasesInAllUSStates;
+import com.sabre.labs.gcpdemo.feign.dtos.CasesInAllUSStates;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class COVID19Controller {
-    private final CasesInAllUSStateRepository repository;
     private final Covid19FeignClient client;
 
+    private final ObjectMapper mapper;
+
+    private final Storage storage;
+
+    private final static String BUCKET_NAME = "qwiklabs-gcp-03-7197bd37f0a4";
+
+    private void createFile(byte[] file, String fileName) {
+        storage.create(BlobInfo.newBuilder(BUCKET_NAME, fileName + ".json").build(),
+                file);
+    }
+
     @GetMapping(value = "", produces = "application/json")
-    public List<CasesInAllUSStates> getAll() {
-        return (List<CasesInAllUSStates>) repository.findAll();
+    public List<String> getAll() {
+        Bucket bucket = storage.get(BUCKET_NAME);
+        Page<Blob> blobs = bucket.list();
+        return StreamSupport.stream(blobs.getValues().spliterator(), false)
+                .map(blob -> {
+                    log.info("blob name: {}", blob.getName());
+                    return blob.getName();
+                })
+                .collect(Collectors.toList());
     }
 
     @GetMapping(value = "/update", produces = "application/json")
     public List<CasesInAllUSStates> updateAndGetAll() {
         List<CasesInAllUSStates> casesInAllUSStates = client.getAll().get(0).getData().get(0).getTable();
-        return (List<CasesInAllUSStates>) repository.saveAll(casesInAllUSStates);
+        casesInAllUSStates.forEach(cases -> {
+            try {
+                createFile(mapper.writeValueAsBytes(cases), cases.getUSAState());
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+        });
+        return casesInAllUSStates;
     }
-
-    @GetMapping(value = "/find/{state}", produces = "application/json")
-    public List<CasesInAllUSStates> findByStateName(@PathVariable String state) {
-        return repository.findCasesInAllUSStatesByUSAStateIgnoreCaseContaining(state);
-    }
+//
+//    @GetMapping(value = "/find/{state}", produces = "application/json")
+//    public List<CasesInAllUSStates> findByStateName(@PathVariable String state) {
+//        return repository.findCasesInAllUSStatesByUSAStateIgnoreCaseContaining(state);
+//    }
 }
